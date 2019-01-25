9bda25d966b9721f5b31e1b12cd6eab843a71558
Patrick DeVivo <patrick.devivo@gmail.com>
2019-01-25 10:14:46 -0800

    add more test coverage

--- grpc/grpc_test.go
+++ grpc/grpc_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func TestConfigFromEnv(t *testing.T) {
+func TestConfigFromEnvValid(t *testing.T) {
 	bindValue := "GRPC_BIND_TEST_VALUE"
 	portValue := 50060
 	os.Setenv("GRPC_BIND", bindValue)
@@ -20,5 +20,25 @@ func TestConfigFromEnv(t *testing.T) {
 	}
 	if config.Port != portValue {
 		t.Fatalf("expected=%d, got=%d", portValue, config.Port)
+	}
+
+	bindAddr, err := config.BindAddress()
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := "GRPC_BIND_TEST_VALUE:50060"
+	if bindAddr != want {
+		t.Fatalf("error in retrieving BindAddress, want=%s, got=%s", want, bindAddr)
+	}
+}
+
+func TestConfigFromEnvInvalid(t *testing.T) {
+	bindValue := "GRPC_BIND_TEST_VALUE"
+	portValue := "NOT_AN_INT"
+	os.Setenv("GRPC_BIND", bindValue)
+	os.Setenv("GRPC_PORT", portValue)
+	_, err := ConfigFromEnv()
+	if err == nil {
+		t.Fatalf("expected error parsing port value, got: %v", err)
 	}
 }
