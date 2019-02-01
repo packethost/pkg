159fb58c430975ab6d96dd2803dd739060f1db10
Manuel Mendez <mmendez534@gmail.com>
2019-02-01 09:47:06 -0500

    log/rollbar: add tests

---
+++ log/internal/rollbar/rollbar_test.go
@@ -0,0 +1,72 @@
+package rollbar
+
+import (
+	"runtime"
+	"testing"
+
+	"github.com/pkg/errors"
+	rollbar "github.com/rollbar/rollbar-go"
+)
+
+func TestStack(t *testing.T) {
+	pc := make([]uintptr, 10)
+
+	err := errors.New("hi")
+	n := runtime.Callers(1, pc)
+
+	stack := []rollbar.Frame(rError{err: err}.Stack())
+	if n == 0 {
+		t.Fatal("unable to recover stack information")
+	}
+	pc = pc[:n]
+	frames := runtime.CallersFrames(pc)
+
+	// Loop to get frames.
+	// A fixed number of pcs can expand to an indefinite number of Frames.
+	i := 0
+	for {
+		want, more := frames.Next()
+		if i >= len(stack) {
+			t.Fatalf("stack frame count mismatch, index=%d, gotLength=%d", i, len(stack))
+		}
+		got := stack[i]
+		if i == 0 {
+			want.Line -= 1 // account for calling runtime.Callers on line after errors.New
+		}
+
+		if want.File != got.Filename {
+			t.Fatalf("filename mismatch: i=%d\nwant=%s\n got=%s\n", i, want.File, got.Filename)
+		}
+		if want.Func.Name() != got.Method {
+			t.Fatalf("func name mismatch: i=%d\nwant=%s\n got=%s\n", i, want.Func.Name(), got.Method)
+		}
+		if want.Line != got.Line {
+			t.Fatalf("line number mismatch: i=%d\nwant=%d\n got=%d\n", i, want.Line, got.Line)
+		}
+
+		if !more {
+			break
+		}
+		i++
+	}
+
+	if i < len(stack)-1 {
+		t.Fatalf("stack frame count mismatch, index=%d, got=%d", i, len(stack))
+	}
+}
+
+func TestError(t *testing.T) {
+	err := errors.New("hi")
+	rErr := rError{err: err}
+	if rErr.Error() != err.Error() {
+		t.Fatalf("Error() mismatch:\nwant=%s\n got=%s\n", err.Error(), rErr.Error())
+	}
+}
+
+func TestCause(t *testing.T) {
+	err := errors.New("hi")
+	rErr := rError{err: err}
+	if rErr.Cause() != err {
+		t.Fatalf("Cause() mismatch:\nwant=%v\n got=%v\n", err, rErr.Cause())
+	}
+}
