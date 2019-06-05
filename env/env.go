644f50c3f776207ecb8cefd28c8b37f9342678d1
Manuel Mendez <mmendez534@gmail.com>
2019-06-05 16:53:52 -0400

    add env package
    This package is meant to simulate python's dict.get() call.
    Tinkerbell uses a similar pattern and I want to use this in a
    forth coming package.

---
+++ env/env.go
@@ -0,0 +1,15 @@
+package env
+
+import "os"
+
+// Get retrieves the value of the environment variable named by the key.
+// If the value is empty or unset it will return the first value of def or "" if none is given
+func Get(name string, def ...string) string {
+	if v := os.Getenv(name); v != "" {
+		return v
+	}
+	if len(def) > 0 {
+		return def[0]
+	}
+	return ""
+}
---
+++ env/env_test.go
@@ -0,0 +1,25 @@
+package env
+
+import (
+	"fmt"
+	"os"
+)
+
+func ExampleGet() {
+	name := "some_environment_variable_that_is_not_set"
+	os.Unsetenv(name)
+	fmt.Println(Get(name))
+	fmt.Println(Get(name, "this is the default"))
+	fmt.Println(Get(name, "this is the default", "this one is ignored"))
+	fmt.Println(Get(name, "", "this one is ignored"))
+	os.Setenv(name, "this is the value set")
+	fmt.Println(Get(name))
+	fmt.Println(Get(name, "this is the default"))
+	// Output:
+	//
+	// this is the default
+	// this is the default
+	//
+	// this is the value set
+	// this is the value set
+}
