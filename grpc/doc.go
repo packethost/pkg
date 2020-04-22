f54e67842101556acdcab4203d9e580af9aeaca7
Manuel Mendez <mmendez534@gmail.com>
2020-04-22 11:11:40 -0400

    Ensure all packages have package docs in doc.go

---
+++ grpc/doc.go
@@ -0,0 +1,5 @@
+// Copyright 2020 - 2020, Packethost, Inc and contributors
+// SPDX-License-Identifier: Apache-2.0
+
+// grpc wraps setup of a grpc server with shared defaults for all packet services
+package grpc
