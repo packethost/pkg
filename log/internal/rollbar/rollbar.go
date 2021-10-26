ccb1b1be900d193b3fac532bb2bbb07d9a5e2a94
Manuel Mendez <mmendez@equinix.com>
2021-10-26 22:10:38 +0000

    log/rollbar: simplify getting the ENV & VERSION from env
    Maybe env should support this itself.

--- log/internal/rollbar/rollbar.go
+++ log/internal/rollbar/rollbar.go
@@ -44,27 +44,19 @@ func Notify(err error) {
 }
 
 func getEnvironment() string {
-	pkgEnv := env.Get("ENV", env.Get("EQUINIX_ENV"))
-	if pkgEnv == "" {
-		// EQUINIX_ENV was not set! Checking for PACKET_ENV - Packet no longer exists, please switch
-		pkgEnv = env.Get("ENV", env.Get("PACKET_ENV"))
-	}
-
+	pkgEnv := env.Get("ENV", env.Get("EQUINIX_ENV", env.Get("PACKET_ENV")))
 	if pkgEnv == "" {
 		log.Panicw("required envvar(ENV) is unset", "envvar", "ENV")
 	}
+
 	return pkgEnv
 }
 
 func getVersion() string {
-	version := env.Get("VERSION", env.Get("EQUINIX_VERSION"))
-	if version == "" {
-		// EQUINIX_VERSION was not set! Checking for PACKET_VERSION - Packet no longer exists, please switch
-		version = env.Get("VERSION", env.Get("PACKET_VERSION"))
-	}
-
+	version := env.Get("VERSION", env.Get("EQUINIX_VERSION", env.Get("PACKET_VERSION")))
 	if version == "" {
 		log.Panicw("required envvar(VERSION) is unset", "envvar", "VERSION")
 	}
+
 	return version
 }
